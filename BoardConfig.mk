19f8aac66401d25b84f5aaeb56e07cd2b74f1d73
Jacob Bell <fzservers@gmail.com>
2012-04-27 15:57:41 -0400

    Disable OTA_ASSERT (recoveries are picky when it comes to this and fails to flash) also enable depreciated HC bootloader cmd line for now

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,14 +17,14 @@
 # include acer t20 common
 include device/acer/t20-common/BoardConfigCommon.mk
 
-TARGET_BOOTLOADER_BOARD_NAME := picasso
-TARGET_OTA_ASSERT_DEVICE := picasso,a500
+#TARGET_BOOTLOADER_BOARD_NAME := picasso
+#TARGET_OTA_ASSERT_DEVICE := picasso,a500
 
 # kernel
 TARGET_PREBUILT_KERNEL := device/acer/picasso/prebuilt/kernel
 
 # HC bootloader support (deprecated soon...)
-#BOARD_KERNEL_CMDLINE := mem=1024M@0M vmalloc=256M video=tegrafb console=none debug_uartport=hsport usbcore.old_scheme_first=1 lp0_vec=8192@0x1840c000 tegra_fbmem=8197120@0x3d81c000 brand=acer target_product=a500_ww_gen1 tegraboot=sdmmc gpt gpt_sector=31258623 androidboot.carrier=wifi-only
+BOARD_KERNEL_CMDLINE := mem=1024M@0M vmalloc=256M video=tegrafb console=none debug_uartport=hsport usbcore.old_scheme_first=1 lp0_vec=8192@0x1840c000 tegra_fbmem=8197120@0x3d81c000 brand=acer target_product=a500_ww_gen1 tegraboot=sdmmc gpt gpt_sector=31258623 androidboot.carrier=wifi-only
 
 BOARD_BOOTIMAGE_PARTITION_SIZE     := 8388608
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 5242880
